15f3ce3dca94a23fc47fa3efd645d376b8f82478
Erik Willems <contact@bitworkz.com>
2018-04-05 15:50:02 +0200

    adyen php library added
    Signed-off-by: Erik Willems <contact@bitworkz.com>

--- src/Client.php
+++ src/Client.php
@@ -2,10 +2,20 @@
 
 namespace bitworkz\Adyen;
 
+/**
+ * Adyen client class
+ * @author erikwillems
+ *
+ */
 class Client
 {
     public function sayHello()
     {
         return 'say hello';
+    }
+    
+    public test()
+    {
+        return 'test';
     }
 }
